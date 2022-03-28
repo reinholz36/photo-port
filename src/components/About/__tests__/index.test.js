04e0a3e20e97041ebee0bb26d7833af342014257
reinholz36 <dreinholz28@gmail.com>
2022-03-28 17:20:24 -0400

    added nav tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,4 +1,5 @@
-import React from "react";
+// __tests__/About.test.js
+import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
@@ -6,14 +7,13 @@ import About from '..';
 afterEach(cleanup);
 
 describe('About component', () => {
-    // First Test 
-    it('renders', () => {
-        render(<About />);
-    });
-    // Second Test
-    it('matches snapshot DOM node structure', () => {
-        // render About
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    })
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
