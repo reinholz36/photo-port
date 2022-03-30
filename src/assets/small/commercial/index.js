f093a9b2cd578a8bd52f778d5d2c8f1174a8b316
reinholz36 <dreinholz28@gmail.com>
2022-03-30 17:01:44 -0400

    added gallery component

---
+++ src/assets/small/commercial/index.js
@@ -0,0 +1,18 @@
+import photoZero from "./0.jpg";
+import photoOne from "./1.jpg";
+import photoTwo from "./2.jpg";
+import photoThree from "./3.jpg";
+import photoFour from "./4.jpg";
+
+export const banana = "help me banana"
+
+const photos = [
+    photoZero,
+    photoOne,
+    photoTwo,
+    photoThree,
+    photoFour
+]
+
+export default photos
+
